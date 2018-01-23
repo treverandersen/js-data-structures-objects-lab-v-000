24f01d6a1d6ac911bb16199812ece0a42f2f3af5
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-23 22:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   driver
 }
