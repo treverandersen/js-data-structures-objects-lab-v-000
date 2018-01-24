da9124f58236b2ba37e5e541163356626ad3bea3
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-24 00:40:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver.address = '12 Broadway';
   return driver
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {...driver}
+  delete newDriver.name
+  return newDriver
+}
